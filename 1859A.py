8d36d196f6a0302bfdcbd27bf9b043e03d2e4d2b
Nirav <np5662948@gmail.com>
2026-06-23 09:52:51 +0530

    did one question

---
+++ 1859A.py
@@ -0,0 +1,29 @@
+import sys
+
+
+def solve():
+    n = int(sys.stdin.readline().strip())
+    a = list(map(int , sys.stdin.readline().split()))
+
+    a.sort()
+
+    if a[0] == a[n-1]:
+        print("-1")
+        return
+    
+    else:
+        it = 0
+        while a[it] == a[0]:
+            at += 1
+        b = a[:it]
+        c = a[it:]
+        
+    
+    print(len(b), len(c))
+    print(*b)
+    print(*c)
+
+if __name__=="__main__":
+    l = int(sys.stdin.readline())
+    for _ in range(l):
+        solve()
